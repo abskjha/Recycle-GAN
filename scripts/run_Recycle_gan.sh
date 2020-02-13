516435c4613604c2f8b3693417cc907dbc5ef9b0
Abhishek Jha <ajha@beryl.esat.kuleuven.be>
2020-02-13 15:05:36 +0100

    1334

--- scripts/run_Recycle_gan.sh
+++ scripts/run_Recycle_gan.sh
@@ -1,2 +1,2 @@
 #!./scripts/train_recycle.sh
-python train.py --dataroot /content/faces --name OliverColbert_256_256 --model recycle_gan  --which_model_netG resnet_6blocks --which_model_netP unet_256 --dataset_mode unaligned_triplet  --no_dropout --gpu 2 --identity 0  --pool_size 0 
+python train.py --dataroot /content/faces/ --name OliverColbert_256_256/ --model recycle_gan  --which_model_netG resnet_6blocks --which_model_netP unet_256 --dataset_mode unaligned_triplet  --no_dropout --gpu 2 --identity 0  --pool_size 0 
